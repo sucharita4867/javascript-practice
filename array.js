f27a077fde03b7c9b1cee95dc82b1c307b6498da
sucharita4867 <sucharitasardar7@gmail.com>
2025-04-22 20:07:04 +0530

    complete array basic practice

--- array.js
+++ array.js
@@ -1,1 +1,6 @@
-// ARRAY PROSSES
+// ------------- ARRAY PRACTICE -------------
+
+const numbers = [81, 1, 10, 48, 65];
+const friends = [farish, sabana, racho, anu, sucho];
+const passed = [true, false, true, false, true, false];
+const mixedArray = [65, sucho, true, 48, racho, false];
