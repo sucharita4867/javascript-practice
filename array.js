1ab6825c16a8ec430d8d193b465371a030086e54
sucharita4867 <sucharitasardar7@gmail.com>
2025-04-22 20:24:12 +0530

    array length practice

--- array.js
+++ array.js
@@ -1,6 +1,8 @@
-// ------------- ARRAY PRACTICE -------------
+// ------------- ARRAY QUALiTI -------------
 
 const numbers = [81, 1, 10, 48, 65];
-const friends = [farish, sabana, racho, anu, sucho];
+const friends = ['farish', 'sabana', 'racho', 'anu', 'sucho'];
 const passed = [true, false, true, false, true, false];
-const mixedArray = [65, sucho, true, 48, racho, false];
+const mixedArray = [65, 'sucho', true, 48, 'racho', false];
+
+console.log(mixedArray)
