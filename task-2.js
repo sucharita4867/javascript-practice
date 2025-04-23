d9e9d17db23b823f0ca22f3f85b34197999f539c
sucharita4867 <sucharitasardar7@gmail.com>
2025-04-24 00:17:34 +0530

    task 2 is complete

---
+++ task-2.js
@@ -0,0 +1,9 @@
+const tourist = ['vrindaban', 'mayapur', 'puri'];
+console.log(tourist)
+
+tourist.push('Varanasi')
+console.log(tourist)
+tourist.push('Sundarbans', 'Himalayas')
+console.log(tourist)
+tourist.pop('Himalayas')
+console.log(tourist)
