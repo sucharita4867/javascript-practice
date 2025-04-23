1cd4cdda1ad7845815066c42395069ac65d2896c
sucharita4867 <sucharitasardar7@gmail.com>
2025-04-23 23:56:19 +0530

    task 1 ta complete

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const numbers = [12, 34, 453, 5352, 784, 45, 89, 546, 90897, 98];
-console.log(numbers[1])
+// console.log(numbers[1])
 
 // get element value b index
-const five = numbers[4];
-console.log(five)
+// const five = numbers[4];
+// console.log(five)
 
 // set or update element value by index
 let abc = 34;
---
+++ task-1.js
@@ -0,0 +1,5 @@
+const fruits = ['apple', 'banana', 'mango', 'orange', 'Grapes'];
+console.log(fruits.indexOf('orange'))
+
+fruits[3] = 'jambura';
+console.log(fruits)
