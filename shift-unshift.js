1d7f5cef4ddc0bfcc0efa444034221a326b6c9da
sucharita4867 <sucharitasardar7@gmail.com>
2025-04-23 21:58:07 +0530

    practice shift-unshift is fully complete

--- shift-unshift.js
+++ shift-unshift.js
@@ -1,8 +1,21 @@
-const friends = ['sucho', 'racho', 'anu', 'farish', 'sabana', 'moni', 'sahil'];
-console.log(friends)
+// const friends = ['sucho', 'racho', 'anu', 'farish', 'sabana', 'moni', 'sahil'];
+// console.log(friends)
 
-friends.shift()
-console.log(friends)
+// friends.shift()
+// console.log(friends)
 
-friends.unshift('hasu')
-console.log(friends)
+// friends.unshift('hasu')
+// console.log(friends)
+
+//--------- practice shift-unshift-----------
+
+const number = [11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+console.log(number)
+
+number.shift()
+number.shift()
+console.log(number)
+
+number.unshift(333)
+number.unshift(5555)
+console.log(number)
