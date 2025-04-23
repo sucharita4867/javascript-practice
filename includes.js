e396a4903f80297fcb6ea0b27a6edc3df547c78c
sucharita4867 <sucharitasardar7@gmail.com>
2025-04-23 21:29:41 +0530

    includes practice is fully complete

---
+++ includes.js
@@ -0,0 +1,25 @@
+const friends = ['sucho', 'racho', 'anu', 'farish', 'sabana', 'moni', 'sahil'];
+
+// console.log(friends.includes('anu'))
+// console.log(friends.includes('such'))
+// console.log(friends.includes('mum'))
+
+// if(friends.includes('moni')){
+//       console.log('ghurte jabo')
+// }
+// else{
+//       console.log('bari te thakbo')
+// }
+
+
+const number = [11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+
+console.log(number.includes(14))
+console.log(number.includes(24))
+
+if(number.includes(23)) {
+      console.log('number ta thik')
+}
+else{
+      console.log('na number ta thik noi')
+}
---
+++ isArray.js
@@ -0,0 +1,12 @@
+const friends = ['sucho', 'racho', 'anu', 'farish', 'sabana', 'moni', 'sahil'];
+
+const food = 'pa n tu a';
+
+const num = [];
+
+const age = 34;
+
+console.log(Array.isArray(friends))
+console.log(Array.isArray(num))
+console.log(Array.isArray(age))
+console.log(Array.isArray(food))
