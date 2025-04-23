9cb0b79f74dcd8a4a1353947dc912568c6225d2c
sucharita4867 <sucharitasardar7@gmail.com>
2025-04-23 22:57:18 +0530

    practice push-array is fully complete

--- push-array.js
+++ push-array.js
@@ -4,11 +4,23 @@ age.push(98)
 age.push(70)
 age.push(46, 78, 90)
 age.push(393531)
-console.log(age)
+// console.log(age)
 
 // String push array
 const friends = ['sucho', 'racho', 'anu', 'farish']
+console.log(friends)
 friends.push('sabana')
 friends.push('sahil')
 friends.push('moni')
 console.log(friends)
+
+// number push array
+
+const number = [16, 17, 18, 19, 20];
+console.log(number)
+
+number.push(222)
+number.push(444)
+number.push(333)
+number.push(555)
+console.log(number)
