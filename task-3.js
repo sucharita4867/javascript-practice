7694ccfd57f24eec2b4b4a2cbcc89a510387e776
sucharita4867 <sucharitasardar7@gmail.com>
2025-04-24 00:25:58 +0530

    task 3 is fully complete

---
+++ task-3.js
@@ -0,0 +1,10 @@
+const book = ['The Alchemist', 'To Kill a Mockingbird', 'Pather Panchal', 'javascript'];
+console.log(book)
+console.log(book.includes('javascript'))
+
+if (book.includes('javascript')){
+      console.log('boi ta porbo')
+}
+else{
+      console.log('boi ta porbo na')
+}
