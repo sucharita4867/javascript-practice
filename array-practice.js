46fca6d488a510999d23f140b635b839c9b6823e
sucharita4867 <sucharitasardar7@gmail.com>
2025-05-07 17:03:51 +0530

    if else practise is complete

--- array-practice.js
+++ array-practice.js
@@ -37,3 +37,27 @@ let numbers = [10, 20, 30, 40, 50];
 // task 12
 // const one = numbers[0];
 // console.log(one)
+
+// task 13
+// console.log(numbers.length- 3)++++++++++++++++++++++++++++++++++++++++++++++==
+
+// task 14
+// const three = numbers[2];
+// console.log(three)
+
+// task 15
+// numbers.unshift(60);
+// console.log(numbers)
+
+// task 16
+// numbers.shift()
+// console.log(numbers)
+
+// task 17+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// task 18
+// console.log(numbers.includes(330))
+
+// task 19++++++++++++++++++++++++++++++++++++++++++++++
+
+// task 20++++++++++++++++++++++++++++++++++++++++++++++++++++++++
---
+++ if-practice.js
@@ -0,0 +1,54 @@
+// ------------------------if else------------------------------
+// task 1
+// let num = 100 ;
+// if(num >= 10) {
+//       console.log('Big :', num)
+// }
+// else if (num < 10 ){
+//       console.log('Small :', num)
+// }
+// task 2
+// let num = 19 ;
+// if (num % 2 === 0){
+//       console.log('Even :', num)
+// }
+// else {
+//       console.log('Odd :', num)
+// }
+// task 3
+// let age = 25 ;
+// if(age >= 18){
+//       console.log('You are an adult :' , age)
+// }
+// else {
+//       console.log('You are not an adult :', age)
+// }
+// task 4
+// let number1 = 94 ;
+// let number2 = 34 ;
+// if(number1 > number2) {
+//       console.log('eta boro :', number1)
+// }
+// else if(number1 < number2){
+//       console.log('eta boro', number2)
+// }
+// else if(number1 === number2 ){
+//       console.log('Both are equal')
+// }
+// task 5
+// let marks = 49 ;
+// if (marks >= 80) {
+//       console.log('Grade: A+ -', marks)
+// }
+// else if(marks >= 70) {
+//       console.log('Grade: A -', marks)
+// }
+// else if(marks >= 60) {
+//       console.log('Grade: B+ -', marks)
+// }
+// else if(marks >= 50) {
+//       console.log('Grade: B -', marks)
+// }
+// else{
+//       console.log('Fail')
+// }
