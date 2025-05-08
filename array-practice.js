574c830dbc12f807116c1fda0699e9d05b7d1e84
sucharita4867 <sucharitasardar7@gmail.com>
2025-05-08 15:22:53 +0530

    added array practice work

--- array-practice.js
+++ array-practice.js
@@ -11,8 +11,10 @@
 // let five = numbers[4];
 // console.log(five)
 
-// task 4+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-// bujte parini question
+// task 4
+// let num = [5, 10, 15, 20, 25] ;
+// const three = num[2];
+// console.log(three)
 
 // task 6
 // numbers.push(30)
@@ -22,24 +24,32 @@
 // numbers.shift()
 // console.log(numbers)
 
-// task 8+++++++++++++++++++++++++++++++++++++++++++++++++
-// for ()
+// task 8
+// for of loop ---------
+// let numbersAll = [5, 10, 15, 20, 25] ;
+// for (let  num of numbersAll){
+//       console.log(num)
+// }
 
-// task 9+++++++++++++++++++++++++++++++++++++++++++++++++++++
+// task 9
+// let num = [5, 10, 15, 20, 25] ;
+// console.log(num.includes(15))
 
-// task 10
+// task 10+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++=
 
 // --------------------start new 10 array practice --------------------------------
 // task 11
-let numbers = [10, 20, 30, 40, 50];
+// let numbers = [10, 20, 30, 40, 50];
 // console.log(numbers.length)
 
 // task 12
 // const one = numbers[0];
 // console.log(one)
 
-// task 13
-// console.log(numbers.length- 3)++++++++++++++++++++++++++++++++++++++++++++++==
+// task 13^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+// let num = [5, 10, 15, 20, 25] ;
+// let numbers = num[num.length - 3]
+// console.log(numbers)
 
 // task 14
 // const three = numbers[2];
@@ -53,7 +63,11 @@ let numbers = [10, 20, 30, 40, 50];
 // numbers.shift()
 // console.log(numbers)
 
-// task 17+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// task 17
+// let numbersAll = [5786, 210, 158, 203, 257] ;
+// for (let num of numbersAll){
+//       console.log(num)
+// }
 
 // task 18
 // console.log(numbers.includes(330))
